6f3f8270769df09f58878bd5e714f2b806268f8a
IndigoCarmine <tamanisikaminai@gmail.com>
2024-03-17 09:40:41 +0900

    remove mock class

--- lib/provider.dart
+++ lib/provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'usbcan.dart';
+import 'package:usbcan_plugins/usbcan.dart';
 
 enum Mode {
   dis,
